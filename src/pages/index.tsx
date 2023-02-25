ee091e8a76e289a9c6a19c522d80c242531aa0e2
Taehoya <cth6809@naver.com>
2023-02-24 20:15:17 -0600

    Change Project Structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "src/components/Sidebar/Sidebar";
+
+// interface HomeProps {
+//   expereince: IExp;
+// }
+
+export default function Home() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
