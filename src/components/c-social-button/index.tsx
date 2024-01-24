2ca40e250aa947dbef3408f7fdd620f3181c9dff
Taehoya <cth6809@naver.com>
2024-01-23 19:28:49 -0600

    create social-button component

---
+++ src/components/c-social-button/index.tsx
@@ -0,0 +1,19 @@
+import * as S from './page.styled';
+import Link from 'next/link';
+
+interface Props {
+    title: string;
+    icon: React.ReactNode;
+    href: string;
+}
+
+export default function CSocialButton({ title, icon, href }: Props) {
+    return (
+        <Link href={href}>
+            <S.Container>
+                <S.Icon>{icon}</S.Icon>
+                <S.Title>{title}</S.Title>
+            </S.Container>
+        </Link>
+    );
+}
---
+++ src/components/c-social-button/page.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+export const Container = styled.div`
+    display: flex;
+    padding: 0.5rem;
+    cursor: pointer;
+`;
+
+export const Icon = styled.div`
+    padding-right: 5px;
+    font-size: 6px;
+`;
+
+export const Title = styled.p`
+    display: flex;
+    align-items: center;
+`;
