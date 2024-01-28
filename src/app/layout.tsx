d09d944f3afd9a5c484af7cc9b07ffc59333836a
Taehoya <cth6809@naver.com>
2024-01-28 16:45:46 -0600

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,17 @@ import { GlobalStyle } from '@/styles/GlobalStyle';
 import { theme } from '@/styles/theme';
 import { ThemeProvider } from 'styled-components';
 import localFont from 'next/font/local';
+import { Quicksand } from 'next/font/google';
 
-const subFont = localFont({
-    src: '../assets/fonts/PretendardVariable.woff2',
-    display: 'swap',
-    variable: '--Pretendard-Variable',
+const font = Quicksand({
+    weight: ['500', '700'],
+    preload: false,
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${subFont.variable}`}>
+            <body className={`${font.className}`}>
                 <StyledComponentsRegistry>
                     <ThemeProvider theme={theme}>
                         <GlobalStyle />
