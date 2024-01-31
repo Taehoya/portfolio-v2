646ff197af27d0d2546e258f5a8405aae871d64b
Taehoya <cth6809@naver.com>
2024-01-30 22:22:27 -0600

    update social button styling

--- src/components/c-social-button/page.styled.ts
+++ src/components/c-social-button/page.styled.ts
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
-import Link from 'next/link';
 
 export const Container = styled.div`
     display: flex;
     padding: 0.5rem;
     cursor: pointer;
+    margin-bottom: 5px;
 `;
 
 export const Icon = styled.div`
-    padding-right: 5px;
+    padding-right: 6px;
     font-size: 6px;
 `;
 
 export const Title = styled.p`
     display: flex;
     align-items: center;
+    font-size: 15px;
 `;
